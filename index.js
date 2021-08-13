25b11d18b84f52aba9584590af9b0434f1922d20
m-babita <babita.majumdar.1@gmail.com>
2021-08-13 10:00:10 +0530

    razorpay first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const Razorpay = require('razorpay')
+
+let app = express()
+
+//in order to setup api key
+const razorpay = new Razorpay({
+    key_id:'rzp_test_aOwE6JvNP9xbw3',
+    key_secret:'v3PcjYLpV7l7o7PX9FUzjnvw',  
+})
+
+
+// initialising after installing ejs
+
+app.set('views','views')
+app.set('view engine','ejs')
+app.use(express.urlencoded({extended: false}))
+
+//to check if its working
+app.get('/',(req,res) => {
+
+    res.render('razorpay.ejs')
+
+})
+
+//a post request for to get order id
+app.post('/order',(req,res) => {
+    let options = {
+        amount: 50000,  // amount in the smallest currency unit
+        currency: "INR",
+        receipt: "order_rcptid_11"
+      };
+
+      razorpay.orders.create(options,(err,order) => {
+          console.log(order)
+      })
+})
+
+app.listen(5000)
