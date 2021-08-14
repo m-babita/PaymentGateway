7f4655b270673f50945de11de5cdcd386eb9bdcf
m-babita <babita.majumdar.1@gmail.com>
2021-08-14 20:05:22 +0530

    Payment gateway and email invoice

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 const express = require('express')
-const Razorpay = require('razorpay')
+
 
 
 let app = express()
-
-//routes
-app.use('/',require('./routes/getdetails'));
-
-//in order to setup api key
-const razorpay = new Razorpay({
-    key_id:'rzp_test_aOwE6JvNP9xbw3',
-    key_secret:'v3PcjYLpV7l7o7PX9FUzjnvw',  
-})
 
 
 // initialising after installing ejs
@@ -26,34 +17,5 @@ app.get('/',(req,res) => {
     res.render('razorpay.ejs')
 
 })
-
-//a post request for to get order id
-app.post('/order',(req,res) => {
-    let options = {
-        amount: 50000,  // amount in the smallest currency unit
-        currency: "INR",
-        receipt: "order_rcptid_11"
-      };
-
-      razorpay.orders.create(options,function (err,order) {
-          console.log(order)
-          res.json(order)
-      })
-})
-
-app.post('/is-order-complete',(req,res)=>{
-
-    razorpay.payments.fetch(req.body.razorpay_payment_id).then((paymentDoc) => {
-        if(paymentDoc.status=='captured'){
-            res.send('Your payment is successful')
-        }
-        else{
-            res.redirect('/')
-        }
-
-    })
-
-})
-
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log(`Server started on Port ${port}`));
